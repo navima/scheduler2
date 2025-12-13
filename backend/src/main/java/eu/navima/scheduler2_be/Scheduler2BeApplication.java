e62543ae9255eb9aebf42240a23f51ef2b2095cb
navima <11299135+navima@users.noreply.github.com>
2025-12-13 21:37:40 +0100

    add be

---
+++ backend/src/main/java/eu/navima/scheduler2_be/Scheduler2BeApplication.java
@@ -0,0 +1,13 @@
+package eu.navima.scheduler2_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scheduler2BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Scheduler2BeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/eu/navima/scheduler2_be/Scheduler2BeApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.navima.scheduler2_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Scheduler2BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
