0ffb79559eed473e1adcde92bcb7b2353af37113
navima <11299135+navima@users.noreply.github.com>
2026-09-15 22:22:00 +0200

    add logo

--- frontend/src/components/TopHeader.tsx
+++ frontend/src/components/TopHeader.tsx
@@ -1,7 +1,15 @@
+import logo from '../assets/logo.svg';
+
 export function TopHeader({ username, handleLogout }: { username: string | undefined | null, handleLogout: () => void }) {
     return <>
         <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '20px' }}>
-            <div>{username}</div>
+            <span style={{ display: 'flex' }}>
+                <a href={"/"}>
+                    <img src={logo} width={32} />
+                </a>
+                <div style={{width: '16px'}}></div>
+                <div>{username}</div>
+            </span>
             {username && <a href="#" onClick={(e) => { e.preventDefault(); handleLogout(); }} style={{ color: '#1976d2', cursor: 'pointer' }}>
                 Logout
             </a>}
