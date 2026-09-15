614bf8ae5ae1c927a2ee9a877136582ec0503e83
navima <11299135+navima@users.noreply.github.com>
2026-09-15 15:29:31 +0200

    format

--- frontend/src/components/TopHeader.tsx
+++ frontend/src/components/TopHeader.tsx
@@ -1,10 +1,10 @@
 export function TopHeader({ username, handleLogout }: { username: string | undefined | null, handleLogout: () => void }) {
     return <>
-    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '20px' }}>
-        <div>{username}</div>
-        {username && <a href="#" onClick={(e) => { e.preventDefault(); handleLogout(); }} style={{ color: '#1976d2', cursor: 'pointer' }}>
-          Logout
-        </a>}
-      </div>
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '20px' }}>
+            <div>{username}</div>
+            {username && <a href="#" onClick={(e) => { e.preventDefault(); handleLogout(); }} style={{ color: '#1976d2', cursor: 'pointer' }}>
+                Logout
+            </a>}
+        </div>
     </>
 }
